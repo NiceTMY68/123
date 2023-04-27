a4550753c6e8c1aa08f73123e64daf7e52c6b2c3
PhongHoang68 <phonghnggch210556@fpt.edu.vn>
2023-04-27 22:59:31 +0700

    dsfd

---
+++ app/Http/Controllers/ModelController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repository\AnimalRepos;
+use Illuminate\Http\Request;
+
+class ModelController extends Controller
+{
+    public function index()
+    {
+        $animal = AnimalRepos::getAllAnimalsWithCategories();
+        return view('Zoo.clientView.index',
+            [
+                'animal' => $animal,
+            ]);    }
+
+
+}
